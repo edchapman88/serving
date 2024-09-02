eeb3a529c922f6d464e7ea242206f5efd748d442
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-09-02 17:27:42 +0100

    add flake.nix and default.nix

---
+++ default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  mkDerivation = {
+    src = builtins.fetchGit {
+      shallow = true;
+      url = "https://github.com/edchapman88/serving";
+    };
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      fetchFromGitHub
+      stdenv
+      ;
+  };
+
+  nodejs-package-lock-v3 = {
+    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+  };
+
+  name = "serving";
+  version = "main";
+}
