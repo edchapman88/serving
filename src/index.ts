c4c67e9e5e5f5ed258954f366174495e14752e78
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-20 17:07:38 +0000

    optionally pass custom port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 import { createServer } from 'node:http';
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = (Number(process.argv[2]) || 3000);
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
